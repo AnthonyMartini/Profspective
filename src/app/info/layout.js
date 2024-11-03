71b70b4055cbe768f568bad16fa93018783c1c80
Anthony Martini <anthonycm2022@gmail.com>
2024-11-02 23:33:46 -0400

    monetage

--- src/app/info/layout.js
+++ src/app/info/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="monetag" content="6bff7ae7ae4d17ff2f03ef969857992f" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3188211813377663"
