c58bdb768dc556acb0d4a67f397cfe43c7360de7
Anthony Martini <anthonycm2022@gmail.com>
2024-11-02 23:35:59 -0400

    monetage

--- src/app/info/layout.js
+++ src/app/info/layout.js
@@ -9,6 +9,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <script
+          src="https://alwingulla.com/88/tag.min.js"
+          data-zone="111847"
+          async
+          data-cfasync="false"
+        ></script>
         <meta name="monetag" content="6bff7ae7ae4d17ff2f03ef969857992f" />
         <script
           async
